a4ee3caf903884ab2f709dad32685056eeffc082
alexi <panahovisgandar@gmail.com>
2026-07-02 20:10:37 +0400

    W2: add Location type and locations.json mock data - closes #20

---
+++ web/types/location.ts
@@ -0,0 +1,13 @@
+export interface Location {
+  id: string;
+  name: string;
+  center: [number, number];
+  bounds: [[number, number], [number, number]];
+  scene_date: string;
+  model: string;
+  flooded_area_km2: number;
+  flooded_pct: number;
+  mask_url: string | null;
+  sar_url: string | null;
+  geotiff_url: string | null;
+}
